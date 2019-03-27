a899d976799cf67d22d1274151535b2229fb2bee
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-27 15:44:22 +0300

    feat: add fromEvent on button

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-var stream$ = rxjs.Observable.create(function(observer) {
-  observer.next(1);
-  observer.next(2);
-  setTimeout(() => {
-    observer.next(3);
-  }, 3000);
-  observer.next(4);
-  observer.complete();
-});
+const button = document.querySelector('button');
 
-stream$
-  .subscribe(
-    function(data) {
-      console.log(`Subscribe: ${data}`);
-    },
-    function(error) {
-      console.log(`Error: ${error}`);
-    },
-    function(completed) {
-      console.log(`Completed!`);
-    }
-  );
+const btn$ = rxjs.fromEvent(button, 'click');
 
-  // --> console.log  1, 2, 4, Completed!
+btn$.subscribe((e) => console.log(e));
