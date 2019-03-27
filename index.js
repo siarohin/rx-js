032cf3ac2e003455cc7f601b4663a1eda140141a
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-27 15:01:47 +0300

    feat: add Subscribe, Error, Completed to subscribe

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var stream$ = rxjs.Observable.create(function(observer) {
   observer.next(1);
-  observer.complete();
+  observer.next(2);
   setTimeout(() => {
     observer.next(3);
   }, 3000);
   observer.next(4);
+  observer.complete();
 });
 
-stream$.subscribe(function(data) {
-  console.log(data); // console.log --> 1
-});
+stream$
+  .subscribe(
+    function(data) {
+      console.log(`Subscribe: ${data}`);
+    },
+    function(error) {
+      console.log(`Error: ${error}`);
+    },
+    function(completed) {
+      console.log(`Completed!`);
+    }
+  );
+
+  // --> console.log  1, 2, 4, Completed!
