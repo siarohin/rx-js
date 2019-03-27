165fbd46f49bc6158578e249a7540f3d54898cbe
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-27 16:03:14 +0300

    feat: add rxjs.interval()

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-rxjs.of(5, 'qwerty', 101, [4, 2, 45])
-  .subscribe(
-      x => console.log(`Next: ${x}`),
-      (error) => console.log(`Error: ${error}`),
-      () => console.log('Completed!')
-    );
+function createSubscribe(name = '') {
+  return {
+    next(x) {
+      console.log(name, ': ', x);
+    },
+    error(err) {
+      console.log(`Error: ${err}`);
+    },
+    complete() {
+      console.log(name, ': Completed');
+    }
+  };
+}
+
+rxjs.interval(1000)
+  .subscribe(createSubscribe('interval'));
