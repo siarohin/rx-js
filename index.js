63fc8ae99b2a5619e582a670672be560404a4c64
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-28 10:44:41 +0300

    feat: add rxjs.concat()

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ function createSubscribe(name = '') {
 }
 
 
-rxjs.range(1, 3)
-    .pipe(rxjs.operators.map(x => rxjs.range(1, 3)))
-    .pipe(rxjs.operators.mergeAll())
-    .subscribe(createSubscribe('merge'));
+// rxjs.range(1, 3)
+//     .pipe(rxjs.operators.map(x => rxjs.range(1, 3)))
+//     .pipe(rxjs.operators.mergeAll())
+//     .subscribe(createSubscribe('merge'));
+
+const s1$ = rxjs.from([1, 2, 3]);
+const s2$ = rxjs.from([4, 5, 6]);
+
+rxjs.concat(s1$, s2$)
+    .subscribe(createSubscribe('concat'));
