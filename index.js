134886850a6fb522a4489b08bc91bd7fc873afa1
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-28 12:27:36 +0300

    feat: add subject

--- index.js
+++ index.js
@@ -12,10 +12,15 @@ function createSubscribe(name = '') {
   };
 }
 
-const s1$ = rxjs.throwError(new Error('Something was wrong!'));
 
-const s2$ = rxjs.interval(500)
-    .pipe(rxjs.operators.take(2));
+const subject$ = new rxjs.Subject();
 
-s1$.pipe(rxjs.operators.onErrorResumeNext(s2$))
-    .subscribe(createSubscribe('onErrorResumeNext'));
+subject$.subscribe(createSubscribe('subject'));
+
+subject$.next(1);
+subject$.next(5);
+
+setTimeout(() => {
+  subject$.complete();
+}, 3000);
+
