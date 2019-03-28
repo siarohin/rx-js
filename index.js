60fe5e5f286bb177de66707384868ec2c66dd9f9
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-28 10:53:27 +0300

    feat: add rxjs.range()... concatAll()

--- index.js
+++ index.js
@@ -13,13 +13,10 @@ function createSubscribe(name = '') {
 }
 
 
-// rxjs.range(1, 3)
-//     .pipe(rxjs.operators.map(x => rxjs.range(1, 3)))
-//     .pipe(rxjs.operators.mergeAll())
-//     .subscribe(createSubscribe('merge'));
-
 const s1$ = rxjs.from([1, 2, 3]);
 const s2$ = rxjs.from([4, 5, 6]);
 
-rxjs.concat(s1$, s2$)
-    .subscribe(createSubscribe('concat'));
+rxjs.range(1, 3)
+    .pipe(rxjs.operators.map(x => rxjs.range(x, 3)))
+    .pipe(rxjs.operators.concatAll())
+    .subscribe(createSubscribe('concatAll'));
