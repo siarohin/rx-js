8903511ebb84d1aa93b4ad68475010b0cab42a25
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-28 14:48:38 +0300

    feat: complite course

--- index.js
+++ index.js
@@ -6,9 +6,25 @@ function getUserById(id) {
   };
 
   return $.ajax({
-    url: 'https://api.vk.com/method/users.get?' + $.param(params),
+    url: 'https://api.vk.com/method/users.get?' + $.param(params) + '&v=5.92',
     type: 'GET',
     dataType: 'JSONP'
     }).promise();
 }
+
+
+rxjs.fromEvent($('.main__input'), 'keyup')
+    .pipe(rxjs.operators.pluck('target', 'value'))
+    .pipe(rxjs.operators.distinct())
+    .pipe(rxjs.operators.debounceTime(2000))
+    .pipe(rxjs.operators.mergeMap(value => getUserById(value)))
+    .pipe(rxjs.operators.map(x => x.response[0]))
+    .subscribe(
+        (user) => {
+          $('.info-title').html(`${user.first_name} ${user.last_name} <span class="info-id">uid: ${user.id}</span>`);
+          $('.info-photo').attr('src', user.photo_big);
+        },
+        error => console.log(error),
+        () => console.log('Completed')
+    );
 
