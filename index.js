ebea3821893d98bbf27a38f18d4444cd3a486991
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-28 13:37:32 +0300

    feat: add API settings

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function createSubscribe(name = '') {
-  return {
-    next(x) {
-      console.log(name, ': ', x);
-    },
-    error(err) {
-      console.log(`Error: ${err}`);
-    },
-    complete() {
-      console.log(name, ': Completed');
-    }
+function getUserById(id) {
+  const params ={
+    access_token: 'b3415dcf245787a12cf88a1682c62e03620655bb780c4d2515f101d6a469a6dd62b154e3184992919e242',
+    user_ids: id,
+    fields: 'photo_big',
   };
+
+  return $.ajax({
+    url: 'https://api.vk.com/method/users.get?' + $.param(params),
+    type: 'GET',
+    dataType: 'JSONP'
+    }).promise();
 }
+
