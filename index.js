7ed59d0260c1b8363c0a5f97d1973661599a6b6a
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-28 10:58:17 +0300

    feat: add rxjs.of() and 2 subscribe (incorrect!)

--- index.js
+++ index.js
@@ -13,10 +13,8 @@ function createSubscribe(name = '') {
 }
 
 
-const s1$ = rxjs.from([1, 2, 3]);
-const s2$ = rxjs.from([4, 5, 6]);
-
-rxjs.range(1, 3)
-    .pipe(rxjs.operators.map(x => rxjs.range(x, 3)))
-    .pipe(rxjs.operators.concatAll())
-    .subscribe(createSubscribe('concatAll'));
+rxjs.of('Hello')
+    // .pipe(rxjs.operators.map(x => rxjs.range(x, 3)))
+    .subscribe(x => rxjs.of(`${x} World`)
+                        .subscribe(createSubscribe('mergeMap'))
+    );
