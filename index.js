cb2d9f6a5b5e85ef8a00b20065487dfc07c1610d
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-03-28 11:25:02 +0300

    feat: rxjs.range()... concatMap()

--- index.js
+++ index.js
@@ -12,14 +12,18 @@ function createSubscribe(name = '') {
   };
 }
 
-const promise = (data) => {
-  return new Promise((res, rej) => {
-    setTimeout(() => {
-      res(`${data} wish you good luck!`);
-    }, 2000);
-  });
-};
+// const promise = (data) => {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       res(`${data} wish you good luck!`);
+//     }, 2000);
+//   });
+// };
 
-rxjs.of('WFM')
-    .pipe(rxjs.operators.mergeMap(x => promise(x)))
-    .subscribe(createSubscribe('promise'))
+rxjs.range(1, 10)
+    .pipe(rxjs.operators.concatMap((x, i) => {
+      return rxjs.interval(100)
+                  .pipe(rxjs.operators.take(x))
+                  .pipe(rxjs.operators.map(y => i));
+    }))
+    .subscribe(createSubscribe('concatMap'));
